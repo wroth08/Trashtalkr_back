9364fd732286397ded412c9f299ee8029aae9148
Tyler Torres <tyler.john.torres@gmail.com>
2017-10-30 15:19:21 -0600

    Added Readme

---
+++ Readme.md
@@ -0,0 +1,13 @@
+# Backend Routes for the app's DB
+
+# Messages Routes
+## Get all messages
+## Get by messages id
+## Post message by id
+
+# Conversations Routes
+## Get by user id
+
+
+## ERD
+![](img/1.png)
--- routes/messages.js
+++ routes/messages.js
@@ -1,40 +1,36 @@
-const router = (module.exports = require('express').Router());
-var knex = require('../knex')
-
+const router = (module.exports = require("express").Router());
+var knex = require("../knex");
 
 // Get all messages
-router.get('/', function (req, res) {
-  knex.select('id', 'string')
-    .from('message')
-    .then( data => {
-      res.json(data)
-    })
-})
+router.get("/", function(req, res) {
+  knex
+    .select("id", "string")
+    .from("message")
+    .then(data => {
+      res.json(data);
+    });
+});
 
 // Get message by id
-router.get('/:id', function (req, res) {
-  let messageId = req.params.id
-  knex.select('string')
-    .from('message')
-    .where('id', messageId)
-    .then( function (data) {
-      res.json(data)
-    })
-})
-//
-// {
-//   'messageID': 2,
-//   'sent_from': 1,
-//   'sent_to': 2,
-// }
+router.get("/:id", function(req, res) {
+  let messageId = req.params.id;
+  knex
+    .select("string")
+    .from("message")
+    .where("id", messageId)
+    .then(function(data) {
+      res.json(data);
+    });
+});
 
 // Create a message that is sent to the user.
-router.post('/', function (req, res) {
-  let messageID = req.body.messageID
-  knex.select('string')
-    .from('message')
-    .where('message.id', messageId)
-    .then( function (data) {
-      insertData['']
-    })
-})
+router.post("/", function(req, res) {
+  let messageID = req.body.messageID;
+  knex
+    .select("string")
+    .from("message")
+    .where("message.id", messageId)
+    .then(function(data) {
+      insertData[""];
+    });
+});
