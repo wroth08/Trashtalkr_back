fdc69ec003e469f27069f977ca47a2c3e7f79450
Jason Dulin <jas.dulin@gmail.com>
2017-10-31 11:21:29 -0700

    Conversations get route working.

--- routes/conversations.js
+++ routes/conversations.js
@@ -9,21 +9,31 @@ router.get('/:id', function (req, res) {
       this.on('message.id', 'users_message.message_id')
     })
     .join('users', function () {
-      this.on('users_message.sent_to', 'users.id')
+      this.on('users_message.sent_from', 'users.id')
     })
-    .where('users.id', myId)
+    .where('users_message.sent_to', myId)
     .then( function (data) {
       res.json(data)
     })
+    // .then( function (data) {
+    //   let dataSoFar = data
+    //   var sent_from = data[0]['sent_from']
+    //   knex.select('users.name')
+    //     .from('users')
+    //     .where('users.id', sent_from)
+    //     .then( function (data) {
+    //       res.json(data)
+    //     })
+    // })
 })
 
 // Create a message that is sent to the user.
-// router.post('/', function (req, res) {
-//   let messageRow = req.body
-//   knex('message')
-//     .insert(messageRow)
-//     .returning('*')
-//     .then( () => {
-//       res.json('message')
-//     })
-// })
+router.post('/', function (req, res) {
+  let messageRow = req.body
+  knex('message')
+    .insert(messageRow)
+    .returning('*')
+    .then( () => {
+      res.json('message')
+    })
+})
